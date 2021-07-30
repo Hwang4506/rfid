a8e3a0bc78d3f38ab115578641332eb257e242d4
황용근 <dydrms4506@naver.com>
2021-07-30 11:13:41 +0900

    시나리오1

--- rfid/forms.py
+++ rfid/forms.py
@@ -6,17 +6,17 @@ from rfid.models import Material_Warehouse, Production_Line, Finished_Product_Wa
 class ProductionForm(forms.ModelForm):
     class Meta:
         model = Production_Line
-        fields = ['Inline_Date', 'RFID_Number_pl', 'Quantity_pl', "Location_pl1"]
+        fields = ['Inline_Date', 'RFID_Number_pl', 'Quantity_pl']
 
 class MaterialForm(forms.ModelForm):
     class Meta:
         model = Material_Warehouse
-        fields = ['Receiving_Date_mw', 'RFID_Number_mw', 'Quantity_mw', "Location_mw1"]
+        fields = ['Receiving_Date_mw', 'RFID_Number_mw', 'Quantity_mw']
 
 class FinishedForm(forms.ModelForm):
     class Meta:
         model = Finished_Product_Warehouse
-        fields = ['Receiving_Date_fw', 'RFID_Number_fw', 'Quantity_fw', "Location_fw1"]
+        fields = ['Receiving_Date_fw', 'RFID_Number_fw', 'Quantity_fw']
 
 class DirectionForm(forms.ModelForm):
     class Meta:
